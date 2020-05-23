8548ff8aece4977776b2bfce4bc6568bf9853c8a
cizuor <cizuor83@free.fr>
2020-05-24 00:05:06 +0200

    premier push
    premier push

---
+++ JDR/JDR/ClientApp/src/app/GestionPerso/new-perso/new-perso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-perso',
+  templateUrl: './new-perso.component.html',
+  styleUrls: ['./new-perso.component.css']
+})
+export class NewPersoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
